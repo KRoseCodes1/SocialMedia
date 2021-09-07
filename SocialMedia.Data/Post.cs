da2f97911fdf19089a2f7fad3f3cd56962983877
Kacie Kasner <krosecodes@gmail.com>
2021-09-07 19:27:10 -0400

    Post class and Post Service created, PostController in progress

---
+++ SocialMedia.Data/Post.cs
@@ -0,0 +1,34 @@
+﻿using SocialMedia.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Models
+{
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Text { get; set; }
+        public virtual List<Comment> Comments {
+            get {
+                return _comments;
+            }
+            set {
+                using (var ctx = new ApplicationDbContext())
+                {
+                    var _comments = ctx.Comments.Where(e => e.AuthorId == AuthorId).Select(e => new CommentListItem { Id = e.Id, Text = e.Text });
+                }
+            }
+        }
+        public virtual List<Like> Likes { get; set; }
+        [Required]
+        public Guid AuthorId { get; set; }
+    }
+}
---
+++ SocialMedia_Api/Controllers/PostController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNet.Identity;
+using SocialMedia.Models;
+using SocialMedia.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SocialMedia_Api.Controllers
+{
+    [Authorize]
+    public class PostController : ApiController
+    {
+        private PostService CreatePostService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var postService = new PostService(userId);
+            return postService;
+        }
+        public IHttpActionResult Get()
+        {
+            PostService postService = CreatePostService();
+            var posts = postService.GetPosts();
+            return Ok(posts);
+        }
+        public IHttpActionResult Post(PostCreate post)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+        }
+    }
+}

3621c94e77cf100e798d85c2d1a9cd35a04d42ac
Olaniyi Atanda <olaniyiatanda12@gmail.com>
2021-09-09 19:32:52 -0400

    CommentMethodAdded

--- SocialMedia.Data/Post.cs
+++ SocialMedia.Data/Post.cs
@@ -2,45 +2,21 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SocialMedia.Models
+
+namespace SocialMedia.Data
 {
     public class Post
     {
-        [Key]
-        public int Id { get; set; }
-        [Required]
+        [ForeignKey(nameof(Posts))]
+        public int PostId { get; set; }
+        public virtual Post Posts { get; set; }
         public string Title { get; set; }
-        [Required]
-        public string Text { get; set; }
-        //public virtual List<Comment> Comments {
-        //    get {
-        //        return _comments;
-        //    }
-        //    set {
-        //        using (var ctx = new ApplicationDbContext())
-        //        {
-        //            var _comments = ctx.Comments.Where(e => e.PostId == Id).Select(e => new CommentListItem { Id = e.Id, Text = e.Text, AuthorId = e.AuthorId });
-        //        }
-        //    }
-        //}
-        //public virtual List<Like> Likes {
-        //    get
-        //    {
-        //        return _likes;
-        //    }
-        //    set
-        //    {
-        //        using (var ctx = new ApplicationDbContext())
-        //        {
-        //            var _comments = ctx.Comments.Where(e => e.PostId == Id).Select(e => new LikeListItem { Id = e.Id, OwnerId = e.OwnerId});
-        //        }
-        //    }
-        //}
-        [Required]
+        public virtual List<Comment> Comments { get; set; }
         public Guid AuthorId { get; set; }
     }
 }
--- SocialMedia.Data/Reply.cs
+++ SocialMedia.Data/Reply.cs
@@ -4,13 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SocialMedia.Models
+namespace SocialMedia.Data
 {
-    public class PostListItem
+    public class Reply
     {
-        public int Id { get; set; }
-        public string Title { get; set; }
-     
+        public int ReplyId { get; set; }
         public string Text { get; set; }
+        public Guid AuthorId { get; set; }
+
     }
 }

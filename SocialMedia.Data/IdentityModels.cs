fa003b09dca8a803ac51f495dcdae2011a985751
KRoseCodes1 <KRoseCodes@gmail.com>
2021-09-09 21:12:07 -0400

    Fixed errors

--- SocialMedia.Data/IdentityModels.cs
+++ SocialMedia.Data/IdentityModels.cs
@@ -6,11 +6,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
+using SocialMedia.Data;
 using SocialMedia.Models;
 
 namespace SocialMedia.Data
 {
-    // Hey, You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager, string authenticationType)
@@ -28,7 +29,7 @@ namespace SocialMedia.Data
             : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
-        
+
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
